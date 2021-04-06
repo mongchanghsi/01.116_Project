4767b6632c48b057f239e8276105d7df9719a828
mongchanghsi <m.changhsi@gmail.com>
2021-04-06 15:53:18 +0800

    Included similarity functions

--- utils/isBrand.py
+++ utils/isBrand.py
@@ -1,3 +1,4 @@
+brand_GT = ['TECNIS', 'Sensar', 'AcrySof']
 brands = ['TECNIS', 'SENSAR', 'ACRYSOF']
 # need to implement similarity score
 def isBrand(x):
@@ -5,3 +6,29 @@ def isBrand(x):
   if x not in brands:
     return False
   return True
+
+def brandSimilarity(x):
+  similarityScore = {}
+  x = x.upper()
+  for i in brands:
+    word, score = similarityFunction(x, i)
+    if word not in similarityScore.keys():
+      similarityScore[word] = score
+  if similarityScore != {}:
+    most_similar_brand = (max(similarityScore, key=similarityScore.get))
+    highest_score = max(similarityScore.values())
+    return most_similar_brand, highest_score
+
+def similarityFunction(word1, word2):
+  # word1 is the OCR
+  # word2 is the 'ground-truth' word
+  count = 0
+  i, j = 0, 0
+
+  while i < len(word1) and j < len(word2):
+    if word1[i] == word2[i]:
+      count += 1
+    i += 1
+    j += 1
+  score = (count / len(word1)) * 100
+  return word2, score
--- utils/isDiopter.py
+++ utils/isDiopter.py
@@ -1,4 +1,15 @@
 def isDiopter(x):
-  if x[0] != '+' or x[len(x)-1] != 'D':
+  # if x[0] != '+' or x[len(x)-1] != 'D':
+  #   return False
+  if x[0] != '+':
     return False
   return True
+
+# Process Diopter checks if there is an additional 'D' or other stuff in the string, if there is, remove it
+def processDiopter(x):
+  while True:
+    if x[len(x)-1] == 'D' or x[len(x)-1].isdigit() == False:
+      x = x[:-1]
+    else:
+      break
+  return x
